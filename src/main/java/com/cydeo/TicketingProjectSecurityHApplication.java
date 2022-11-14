30b43ffd35179f1d08363da6d644cb0fc0957530
Hrvoje <hbrkljacic@gmail.com>
2022-11-13 22:30:27 -0500

    Security

---
+++ src/main/java/com/cydeo/TicketingProjectSecurityHApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectSecurityHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectSecurityHApplication.class, args);
+    }
+
+}
